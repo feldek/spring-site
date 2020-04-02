5246e508b5bd693357e730b7211a4c7b0275f215
Felldek <felldek1@gmail.com>
2020-04-02 16:49:58 +0300

    drop-down field for search

--- js.js
+++ js.js
@@ -39,61 +39,78 @@ function addItemOnPage(arr) {
 addItemOnPage(arrOfItem);
 
 // full height of document
-let scrollHeight = Math.max(
-  document.body.scrollHeight,
-  document.documentElement.scrollHeight,
-  document.body.offsetHeight,
-  document.documentElement.offsetHeight,
-  document.body.clientHeight,
-  document.documentElement.clientHeight
-);
-let top_menu1 = document.querySelector(".top-menu");
-top_menu1.style = `height : ${scrollHeight + 50}px`;
+function max_height_window() {
+  let scrollHeight = Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.body.clientHeight,
+    document.documentElement.clientHeight
+  );
+  let top_menu1 = document.querySelector(".top-menu");
+  top_menu1.style = `height : ${scrollHeight + 50}px`;
+}
+max_height_window();
 
 let locale_HTML = document.querySelector(".main-block").innerHTML;
 function findOnPage(status) {
   function findOnPageGo() {
     let input = document.querySelector(".place_for_search").value;
+    if (input === "") return;
     let inputRegular = "/" + input + "/gi";
     inputRegular.trim();
-
-    let searchElemAll = document.querySelectorAll(".block a");
+    if (input.length < 3) {
+      fill_card_exceptions("Enter more 2 symbols");
+      return;
+    }
+    let searchElemAll = document.querySelectorAll(".frame-text");
     searchElemAll.forEach(elem => {
       let match = false; // indicator true search
       elem.innerHTML = elem.innerHTML.replace(
         eval(inputRegular),
         '<span style="background-color:yellow;">' + input + "</span>"
       );
-      elem.closest("a").classList.toggle("open");
+      elem.closest("a.box-card").classList.toggle("open");
       match = elem.innerHTML.match(
         eval('/<span style="background-color:yellow;">/gi')
       )
         ? true
         : false;
       if (match) {
-        elem.closest("a").classList.toggle("open");
+        elem.closest("a.box-card").classList.toggle("open");
       }
       match = false;
     });
     document.querySelector(".place_for_search").value = "";
+
     let cardClassOpen = document.querySelectorAll(".block a.open");
-    if (searchElemAll.length === cardClassOpen.length) {
-      searchElemAll.forEach(elem => {
-        elem.closest("a").classList.toggle("open");
-        elem.innerHTML =
-          '<div class = "nothing_card_box"><div class = "nothing_card_box">Nothing found</div></div>';
+
+    if (searchElemAll.length === cardClassOpen.length) fill_card_exceptions("Nothing found");
+
+    function fill_card_exceptions(content) {
+      document.querySelectorAll(".block a").forEach(elem => {
+        if(elem.closest("a.box-card").classList[1]) elem.closest("a.box-card").classList.toggle("open");
+        elem.innerHTML = `<div class = "nothing_card_box"><div class = "nothing_card_box">${content}</div></div>`;
       });
     }
+
+    max_height_window();
     openMenu();
   }
   function findOnPageBack() {
     document.querySelector(".main-block").innerHTML = locale_HTML;
   }
+  if (document.querySelector("form.open")) {
+    document.querySelector("form").classList.toggle("open");
+  }
+
   if (status) {
     findOnPageBack();
     findOnPageGo();
   } else if (!status) {
     findOnPageBack();
+    document.querySelector("form").classList.toggle("open");
     document.querySelector(".place_for_search").value = "";
   }
 }
