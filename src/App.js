c1b63ad52bc44f9575e5602b3d7ca6db0ca9cbdf
felldek <felldek1@gmail.com>
2020-05-10 14:44:43 +0300

    global state was moved in hooks(update)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
-import Header from "./Header/Header";
+import Header from "./MainHeader/Header";
 import ContainerContent from "./Content/Project/ContainerContent";
 
 
